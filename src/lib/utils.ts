43218057a379e90f00a143d66df64c3dd35431ba
Oliver Nguyen <nkhanhvan293@gmail.com>
2025-06-05 02:35:46 +0700

    fix connection issues

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,9 +2,14 @@ export const ApiUrl = import.meta.env.VITE_API_URL
 
 export function GetAuthToken(): string | null {
     const cookies = document.cookie.split(';').reduce((acc, c) => {
-        const [key, value] = c.trim().split('=');
+        const index = c.indexOf('=');
+        if (index === -1) return acc;
+
+        const key = c.slice(0, index).trim();
+        const value = c.slice(index + 1);
         acc[key] = value;
         return acc;
     }, {} as Record<string, string>);
+
     return cookies.authToken || null;
 }
